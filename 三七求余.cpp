af009fb9107086068a16e9a4d0bde4cf6d4698bf
Esirn <31148342+Esirn@users.noreply.github.com>
2019-03-08 23:02:14 +0800

    C->Cpp

--- 三七求余.cpp
+++ 三七求余.cpp
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
 	int n;
 	float s=0;
 	printf("请输入一个数\n");
-	scanf("%d",&n);
+	cin>>n;
 	for(;n>0;n--)
 	{
-		if(n%3==0||n%7==0)s += (float)1/n;
+		if(n%3==0||n%7==0)s += float(1)/n;
 	}
-	printf("%f",s);
+	cout<<s;
 	return 0;
 }
