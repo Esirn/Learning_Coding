5e860c551aa529829d8708707fd9b72f8f9a6d94
Esirn <31148342+Esirn@users.noreply.github.com>
2019-03-09 22:54:21 +0800

    TAB->Space

--- 阶乘之和作分母.py
+++ 阶乘之和作分母.py
@@ -1,15 +1,15 @@
 def jc(k): #返回k!。
-	if(k!=1):
-		return k*jc(k-1)
-	else:
-		return 1
+    if(k!=1):
+        return k*jc(k-1)
+    else:
+        return 1
 
 temp=0;
 S=0;
 
 for i in range(1,21,3):
-	temp += jc(i)
-	print("temp={0}".format(temp))
-	S += 1/temp
+    temp += jc(i)
+    print("temp={0}".format(temp))
+    S += 1/temp
 
 print("S={0}".format(S))
